37676d91ef3aad4ce51a066e6ff75338093d1a04
KiRiLL625 <70346582+KiRiLL625@users.noreply.github.com>
2023-05-18 19:11:46 +0300

    Update Animal.cpp

--- Animal.cpp
+++ Animal.cpp
@@ -5,34 +5,34 @@
 #include "Animal.h"
 #include <iostream>
 
-std::string Animal::species;
-
 Animal::Animal(const std::string& name, uint age, const std::string& breed) {
     this->name = name;
     this->age = age;
     this->breed = breed;
 }
 
-std::string Animal::getSpecies(){
-    return Animal::species;
-}
-
 std::ostream& operator<<(std::ostream& out, const Animal& animal) {
-    out << "______________________\n" << animal.say() << "\n" << animal.play() <<
+    out << "_____________________________________\n" << animal.say() << "\n" << animal.play() <<
     "\n\tAnimal: " << animal.name << "\n\tAge: " << animal.age <<
-    "\n\tSpecies: " << Animal::getSpecies() << "\n\tBreed: " << animal.breed <<
-    "\n______________________" << std::endl;
+    "\n\tSpecies: " << animal.getSpecies() << "\n\tBreed: " << animal.breed <<
+    "\n_____________________________________" << std::endl;
     return out;
 }
 
 bool Animal::operator==(const Animal& other) const {
     return this->name == other.name && this->age == other.age &&
-    this->breed == other.breed && getSpecies() == Animal::getSpecies();
+    this->breed == other.breed && this->getSpecies() == other.getSpecies();
 }
+
+const std::string Cat::species = "Cat";
 
 Cat::Cat(const std::string& name, uint age, const std::string& breed):
     Animal(name, age, breed){
-    Animal::species = "Cat";
+    //Cat::species = "Cat";
+}
+
+std::string Cat::getSpecies() const{
+    return species;
 }
 
 std::string Cat::say() const {
@@ -43,12 +43,18 @@ std::string Cat::play() const {
     return "I'm playing with a ball of yarn!";
 }
 
+const std::string Dog::species = "Dog";
+
 Dog::Dog(const std::string& name, uint age, const std::string& breed):
     Animal(name, age, breed){
-    Animal::species = "Dog";
+    //Dog::species = "Dog";
 }
 
-std::string Dog::say() const {
+std::string Dog::getSpecies() const{
+    return species;
+}
+
+std::string Dog::say() const{
     return "Woof!";
 }
 
