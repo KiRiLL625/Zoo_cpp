27d08bb82d215ca4165a3389f6269a3a99d0dc83
KiRiLL625 <70346582+KiRiLL625@users.noreply.github.com>
2023-05-18 19:11:15 +0300

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -11,28 +11,34 @@ typedef unsigned int uint;
 class Animal {
 protected:
     uint age;
-    static std::string species;
+    //static std::string species;
     std::string breed;
     std::string name;
 public:
     Animal(const std::string& name, uint age, const std::string& breed);
     virtual std::string say() const = 0;
     virtual std::string play() const = 0;
-    static std::string getSpecies();
+    virtual std::string getSpecies() const = 0;
     friend std::ostream& operator<<(std::ostream& out, const Animal& animal);
     bool operator==(const Animal& other) const;
 };
 
 class Cat: public Animal {
+private:
+    static const std::string species;
 public:
     Cat(const std::string& name, uint age, const std::string& breed);
+    std::string getSpecies() const override;
     std::string say() const override;
     std::string play() const override;
 };
 
 class Dog: public Animal {
+private:
+    static const std::string species;
 public:
     Dog(const std::string& name, uint age, const std::string& breed);
+    std::string getSpecies() const override;
     std::string say() const override;
     std::string play() const override;
 };
